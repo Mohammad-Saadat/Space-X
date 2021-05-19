609a8820afe7c3842d5c027c858f7047ff4485be
mohammad saadat <m.sdt1376@gmail.com>
2021-05-19 23:41:12 +0430

    Add Options model to project

---
+++ Space-X/Common/Models/LaunchParams.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchParams.swift
+//  Space-X
+//
+//  Created by mohammadSaadat on 2/29/1400 AP.
+//
+
+import Foundation
+
+struct Options: Encodable {
+    // MARK: - Properties
+    let page: Int
+}
